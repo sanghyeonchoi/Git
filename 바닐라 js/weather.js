ab6778fe768b5616a7317877548e3136ed96b0af
sanghyeonchoi <76832243+sanghyeonchoi@users.noreply.github.com>
2021-03-19 16:30:52 +0900

    #3.9 Getting the Weather part Two API

--- 바닐라 js/weather.js
+++ 바닐라 js/weather.js
@@ -1,7 +1,23 @@
 const COORDS = "coords";
+const API_KEY = "ead13315e61be757375f96a1fac189c4";
+const weather = document.querySelector(".js-weather");
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temp = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temp} @${place}`;
+    });
 }
 
 function handleGeosucces(position) {
@@ -12,6 +28,7 @@ function handleGeosucces(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 function handleGeoError() {
   console.log("cant access geo location");
@@ -26,6 +43,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parseCoords = JOSN.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
